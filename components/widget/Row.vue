8a50b93994f9fb8a502b1229d0c3a1673a810b18
yuki-nolea <rentech080@gmail.com>
2025-03-03 00:24:53 +0900

    add: display sample parameter table

--- components/widget/Row.vue
+++ components/widget/Row.vue
@@ -1,13 +1,70 @@
 <template>
-  <div>
-    
+  <div class="
+    grid grid-rows-[1fr] grid-cols-[158px_96px_158px_1fr] 
+    shadow-[0_1px_3px_rgba(0,0,0,0.12),_0_1px_2px_rgba(0,0,0,0.24)]
+    transition-all duration-300 ease-[cubic-bezier(.25,.8,.25,1)]
+    w-full bg-[#fff] text-sm rounded-sm p-1"
+  >
+    <div :class="classObject(nameDiff)" class="name param_cell col-start-1" data-id="">{{ name }}</div>
+    <div v-if="coin == 1" :class="classObject(mandatoryDiff)" class="mandatory param_cell col-start-2" data-id="">{{ mandatory }}</div>
+    <div v-if="coin == 1" :class="classObject(rangeDiff)" class="range param_cell col-start-3" data-id="">{{ range }}</div>
+    <div v-if="coin == 1" :class="classObject(defaultDiff)" class="default param_cell col-start-4" data-id="">{{ default }}</div>
+    <div v-if="coin == 0" :class="classObject(descriptionDiff)" class="description param_cell col-start-2 col-span-3" data-id="">{{ description }}</div>
   </div>
 </template>
 
 <script lang="ts" setup>
+  
+  const props = defineProps<{
+    name: string, nameDiff: string,
+    mandatory: string, mandatoryDiff: string,
+    range: string, rangeDiff: string,
+    default: string, defaultDiff: string,
+    description: string, descriptionDiff: string
+  }>();
 
+  const { coin } = useToggleParam();
+  const classObject = (diffMsg: string) => ({ match:  diffMsg == "match", difference: diffMsg == "difference", alone: diffMsg == "alone", none: diffMsg == "none" });
 </script>
 
 <style scoped>
+  .param_cell
+  {
+    border-right: 1px solid #E0E0E0;
+    text-align: center;
+    overflow-wrap: break-word;
+
+    transition-property: display, opacity, height, padding-top;
+    transition-duration: 0.3s;
+    @starting-style {
+      height: 0px;
+      opacity: 0;
+    }
+  }
+
+  .match
+  {
+    color: black;
+    /* background-color: aqua; */
+  }
+
+  .difference
+  {
+    color: white;
+    background-color: red;
+  }
+
+  .alone
+  {
+    color: white;
+    background-color: #0DDF08;
+  }
+
+  .none
+  {
+    color: white;
+    background-color: #D9D9D9;
+    text-decoration: line-through;
+  }
 
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
 })
