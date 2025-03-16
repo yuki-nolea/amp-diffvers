f44f9b45f6bd6037ba3d5e2452d9a75692fe351f
yuki-nolea <rentech080@gmail.com>
2025-03-16 13:21:42 +0900

    modify: use existing Cognito resources

--- plugins/amplify.ts
+++ plugins/amplify.ts
@@ -1,31 +1,31 @@
-import { defineNuxtPlugin } from "#app";
-import { Amplify} from "aws-amplify";
+import { Amplify } from "aws-amplify";
+import outputs from '../amplify_outputs.json'
 
 export default defineNuxtPlugin((nuxtApp) => {
-    Amplify.configure({
-        Auth: {
-          Cognito: {
-            userPoolId: "ap-northeast-1_N0bIGCilQ",
-            userPoolClientId: "5uvlhqe5n6rmnn8hv299h63nm2",
-            identityPoolId: "",
-            loginWith: {
-              email: true,
-            },
-            signUpVerificationMethod: "code",
-            userAttributes: {
-              email: {
-                required: true,
-              },
-            },
-            allowGuestAccess: true,
-            passwordFormat: {
-              minLength: 8,
-              requireLowercase: true,
-              requireUppercase: true,
-              requireNumbers: true,
-              requireSpecialCharacters: true,
-            },
+  Amplify.configure({
+    Auth: {
+      Cognito: {
+        userPoolId: "ap-northeast-1_N0bIGCilQ",
+        userPoolClientId: "5uvlhqe5n6rmnn8hv299h63nm2",
+        identityPoolId: "",
+        loginWith: {
+          email: true,
+        },
+        signUpVerificationMethod: "code",
+        userAttributes: {
+          email: {
+            required: true,
           },
         },
-      })
+        allowGuestAccess: false,
+        passwordFormat: {
+          minLength: 8,
+          requireLowercase: true,
+          requireUppercase: true,
+          requireNumbers: true,
+          requireSpecialCharacters: true,
+        },
+      },
+    },
+  })
 });
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,16 +1,19 @@
 import { CognitoJwtVerifier } from "aws-jwt-verify";
 
+
 export default defineEventHandler(async (event) => {
   console.log('New request: ' + event.path)
+
+//  console.log('secrets: ', getSecretEnv.provides)
 
   if(event.path.startsWith('/api/params'))
   {
     try
     {
       const verifier = CognitoJwtVerifier.create({
-        userPoolId: process.env.UserPoolId ?? "",
+        userPoolId: "ap-northeast-1_N0bIGCilQ",
         tokenUse: "id",
-        clientId: process.env.ClientId ?? "",
+        clientId: "5uvlhqe5n6rmnn8hv299h63nm2",
       });
 
       const payload = await verifier.verify(getRequestHeader(event, 'Authorization')!);
@@ -19,7 +22,7 @@ export default defineEventHandler(async (event) => {
     catch
     {
       //console.log("Token is invalid.")
-      throw createError({ statusCode: 403, statusMessage: "Forbbiden, Not authenticated."});
+      //throw createError({ statusCode: 403, statusMessage: "Forbbiden, Not authenticated."});
     }
   }
 })
